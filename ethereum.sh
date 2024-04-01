48a32d65da34c488d44662d15cbefdaf717799bb
raydium209 <158184916+raydium209@users.noreply.github.com>
2024-04-01 14:14:56 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-673865ad-a960-4636-8567-a38132bb8b30/"
 
 OUTPUT_DIR="logs"
 
